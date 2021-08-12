50be324a0384398f162d193fbb88f858ad061f19
zgx <1637349696@qq.com>
2021-08-12 17:29:47 +0800

    2.0

--- test_model.py
+++ test_model.py
@@ -18,4 +18,5 @@ for i in range(100):
     respon = requests.post(url="http://127.0.0.1:8000/register", data=json.dumps(data))
     if respon:
         print(respon.json())
-        print("-----------")
+
+
